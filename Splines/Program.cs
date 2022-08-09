ab69d16e7f2307c07df8217cd9eb534341ccbe20
shishnk <akorund@mail.ru>
2022-08-09 23:40:39 +0700

    Добавил свою точку входа Main для последующего добавления инверсии управления и внедрения зависимости

---
+++ Splines/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Splines;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        App app = new();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
