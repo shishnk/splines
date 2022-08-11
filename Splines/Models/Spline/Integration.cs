3b9250e280f2b84f46169d1c34c754e308e9e43a
shishnk <akorund@mail.ru>
2022-08-11 16:32:43 +0700

    Внес поправки

--- Splines/Models/Spline/Integration.cs
+++ Splines/Models/Spline/Integration.cs
@@ -4,9 +4,8 @@ public static class Integration
 {
     public static double GaussOrder5(Func<double, double, double> psi1, Func<double, double, double> psi2, double a, double b)
     {
-        var quadratures = Quadratures.GaussOrder5();
         double h = Math.Abs(b - a);
-        double result = quadratures.Sum(q => q.Weight * psi1((a + b + q.Node * h) / 2.0, h) * psi2((a + b + q.Node * h) / 2.0, h));
+        double result = Quadratures.GaussOrder5().Sum(q => q.Weight * psi1((a + b + q.Node * h) / 2.0, h) * psi2((a + b + q.Node * h) / 2.0, h));
 
         return result * h / 2.0;
     }
