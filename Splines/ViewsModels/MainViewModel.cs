78584d24d85700fd0010aee6bc99433d5cff9ce3
shishnk <akorund@mail.ru>
2022-08-12 23:01:19 +0700

    Добавил NuGet пакет для графика и выделил место в разметке под его отрисовку

--- Splines/ViewsModels/MainViewModel.cs
+++ Splines/ViewsModels/MainViewModel.cs
@@ -1,16 +1,36 @@
-﻿namespace Splines.ViewsModels;
+﻿using OxyPlot;
+using OxyPlot.Series;
+
+namespace Splines.ViewsModels;
 
 public class MainViewModel : ViewModel
 {
     private IDataService _dataService;
+    private FiniteElement _selectedElement;
+    private CollectionViewSource _selectedElementPoints = new();
     private double _alpha = 1E-07;
     private double _beta = 1E-07;
     public ObservableCollection<Point> Points { get; }
     public ObservableCollection<FiniteElement> Elements { get; }
+    public ICollectionView SelectedElementPoints => _selectedElementPoints.View;
+
+    //public FiniteElement SelectedElement
+    //{
+    //    get => _selectedElement;
+    //    set
+    //    {
+    //        if(!Set(ref _selectedElement, value)) return;
+
+    //        _selectedElementPoints.Source = value?.Points;
+    //        OnPropertyChanged(nameof(SelectedGroupStudents));
+    //    }
+    //}
+
     public double Alpha { get => _alpha; set => Set(ref _alpha, value); }
     public double Beta { get => _alpha; set => Set(ref _beta, value); }
     public ICommand CreateElement { get; }
     public ICommand DeleteElement { get; }
+    public PlotModel Graphic { get; }
 
     public MainViewModel(IDataService dataService)
     {
@@ -23,6 +43,9 @@ public class MainViewModel : ViewModel
         var elements = Enumerable.Range(0, 100).Select(i => new FiniteElement(i, i));
 
         Elements = new(elements);
+
+        Graphic = new();
+        Graphic.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
 
         #region Commands
 
